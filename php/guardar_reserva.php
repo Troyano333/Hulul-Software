3d3498988f2e0ab51f220d0c82bb63337894a760
Troyano333 <eldiosapolo10@gmail.com>
2025-05-07 14:08:55 -0500

    actualizacion de login y demas

--- php/guardar_reserva.php
+++ php/guardar_reserva.php
@@ -8,18 +8,18 @@ if ($conexion->connect_error) {
 }
 
 // Verificar datos obligatorios
-if (!isset($_POST['nombre'], $_POST['apellido'], $_POST['palco'], $_POST['email'], $_POST['telefono'], $_POST['fecha'], $_POST['hora'], $_POST['lugar'])) {
+if (!isset($_POST['nombre'], $_POST['apellido'], $_POST['tipo_palco'], $_POST['email'], $_POST['telefono'], $_POST['fecha_reserva'], $_POST['hora_reserva'], $_POST['lugar'])) {
     die("error: Faltan datos obligatorios.");
 }
 
 // Obtener datos
 $nombre = $_POST['nombre'];
 $apellido = $_POST['apellido'];
-$tipo_palco = $_POST['palco'];
+$tipo_palco = $_POST['tipo_palco'];
 $email = $_POST['email'];
 $telefono = $_POST['telefono'];
-$fecha = $_POST['fecha'];
-$hora = $_POST['hora'];
+$fecha = $_POST['fecha_reserva'];
+$hora = $_POST['hora_reserva'];
 $lugar = $_POST['lugar'];
 
 // Validar si la hora ya pasó hoy
@@ -49,17 +49,13 @@ $stmt = $conexion->prepare("INSERT INTO reservas (nombre, apellido, tipo_palco, 
 $stmt->bind_param("ssssssss", $nombre, $apellido, $tipo_palco, $email, $telefono, $fecha, $hora, $lugar);
 
 if ($stmt->execute()) {
-    // Redirigir a la página de confirmación de pago con los datos pasados en la URL
-    header("Location: confirmacion_pago.html?nombre=" . urlencode($nombre) . "&fecha=" . urlencode($fecha) . "&hora=" . urlencode($hora) . "&lugar=" . urlencode($lugar) . "&palco=" . urlencode($tipo_palco));
-    exit;  // Asegúrate de que haya un exit para evitar más ejecución después de la redirección
+    // Redirigir a la página de confirmación de pago con los datos
+    header("Location: ../confirmacion_pago.html?nombre=" . urlencode($nombre) . "&fecha=" . urlencode($fecha) . "&hora=" . urlencode($hora) . "&lugar=" . urlencode($lugar) . "&palco=" . urlencode($tipo_palco));
+    exit;
 } else {
     echo "error: " . $stmt->error;
 }
 
 $stmt->close();
 $conexion->close();
-
-header("Location: ../confirmacion_pago.html");
-exit();
-
 ?>
